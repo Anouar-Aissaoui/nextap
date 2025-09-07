e12aaacfd856791caf81265ebde29ebec3c7d3a6
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-09-07 16:22:11 +0000

    pls update sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -11,6 +11,45 @@ export default function sitemap(): MetadataRoute.Sitemap {
     priority: 0.8,
   }));
 
+  const staticPages = [
+    {
+        url: `${siteUrl}/app`,
+        lastModified: new Date(),
+        changeFrequency: 'weekly' as const,
+        priority: 0.9,
+    },
+    {
+        url: `${siteUrl}/search`,
+        lastModified: new Date(),
+        changeFrequency: 'monthly' as const,
+        priority: 0.5,
+    },
+    {
+        url: `${siteUrl}/privacy-policy`,
+        lastModified: new Date(),
+        changeFrequency: 'yearly' as const,
+        priority: 0.3,
+    },
+    {
+        url: `${siteUrl}/terms`,
+        lastModified: new Date(),
+        changeFrequency: 'yearly' as const,
+        priority: 0.3,
+    },
+    {
+        url: `${siteUrl}/copyright-policy`,
+        lastModified: new Date(),
+        changeFrequency: 'yearly' as const,
+        priority: 0.3,
+    },
+    {
+        url: `${siteUrl}/eu-amau`,
+        lastModified: new Date(),
+        changeFrequency: 'yearly' as const,
+        priority: 0.3,
+    }
+  ];
+
   return [
     {
       url: siteUrl,
@@ -18,6 +57,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily' as const,
       priority: 1,
     },
+    ...staticPages,
     ...appUrls,
   ]
 }
