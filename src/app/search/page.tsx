c676d542b8cdd74a95dfd6054a2253863c9622f3
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-09-07 16:19:11 +0000

    PLS FIX BUG FOR THIS BUG : URL
    Indexable
    
    https://www.appsg.site/search

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,9 +3,14 @@ import { apps } from '@/lib/apps';
 import type { Metadata } from 'next';
 import SearchPage from '@/components/search-page';
 
+const siteUrl = 'https://www.appsg.site';
+
 export const metadata: Metadata = {
   title: 'Search Apps',
   description: 'Search for your favorite modded and tweaked apps for iOS and Android.',
+  alternates: {
+    canonical: `${siteUrl}/search`,
+  },
 };
 
 export default function Search() {
