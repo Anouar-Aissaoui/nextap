8e5bd33440ea09ee06118722e4f8cfb34db762b4
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-09-09 14:02:54 +0000

    PLS NOT DELETE THIS :   <script type="text/javascript">var lck = false;<

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Footer } from '@/components/footer';
 import { Header } from '@/components/header';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -53,6 +54,10 @@ export default function RootLayout({
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://i.imgur.com" />
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `var lck = false;` }} />
+        <script type="text/javascript" src="https://getafilenow.com/script_include.php?id=888063"></script>
+        <script type="text/javascript" dangerouslySetInnerHTML={{ __html: `if(!lck){top.location = 'https://getafilenow.com/help/ablk.php?lkt=1'; }` }} />
+        <noscript><meta http-equiv="refresh" content="0;url=https://getafilenow.com/help/enable_javascript.php?lkt=1" /></noscript>
       </head>
       <body className={inter.className}>
         <div className="relative flex flex-col min-h-screen">
