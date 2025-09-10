ea57b9d32b5f8df480d7aa87885a7185883dccc8
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-09-10 16:22:04 +0000

    yeah

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -46,11 +46,9 @@ export default function PrivacyPolicyPage() {
           <p>
             Your continued use of our website will be regarded as acceptance of our practices around privacy and personal information. If you have any questions about how we handle user data and personal information, feel free to contact us.
           </p>
-          <p>This policy is effective as of 1 January 2026.</p>
+          <p>This policy is effective as of 1 January 2025.</p>
         </CardContent>
       </Card>
     </div>
   );
 }
-
-    
