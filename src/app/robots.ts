59d6098cfd5e7164b7b4c722737b762bcff3a9e9
Anouar Aissaoui <anouaraissaoui.pro@gmail.com>
2025-09-14 16:06:21 +0000

    pls like this : User-Agent: * Allow: / Disallow: /*?q=*

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,3 +1,4 @@
+
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
@@ -7,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
         {
             userAgent: '*',
             allow: '/',
-            disallow: ['/_next/data/'],
+            disallow: ['/_next/data/', '/*?q=*'],
         }
     ],
     sitemap: `${siteUrl}/sitemap.xml`,
